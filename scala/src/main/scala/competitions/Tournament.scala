aa5cc28ab2f7e56d95e27827984a9eb9eb44febc
pspompey <pompeypabl@est.frba.utn.edu.ar>
2020-06-21 02:16:30 -0300

    agrego reglas del torneo

--- scala/src/main/scala/competitions/Tournament.scala
+++ scala/src/main/scala/competitions/Tournament.scala
@@ -7,15 +7,11 @@ case class Tournament(competitions: List[Competition], dragons: List[Dragon], ru
 
   def apply(vikings: List[Viking]): Option[Viking] = {
     competitions.foldLeft(vikings)((competitors, competition) => {
-      if (competitors.length == 1)
-        return competitors.headOption
+      if (competitors.length > 1)
+        rule(competition, dragons, competitors)
       else
-        rule(competition(prepare(competition, competitors)))
+        return competitors.headOption
     }).headOption
-  }
-
-  def prepare(competition: Competition, competitors: List[Viking]): List[Competitor] = {
-    competitors.flatMap(v => v.bestMount(dragons, competition))
   }
 
 }
