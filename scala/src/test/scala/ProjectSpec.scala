5e947ce77f845ce690a41c6cc18f2f1ce4e82d94
fdalmaup <dalmaufacundo@gmail.com>
2020-06-21 20:07:58 -0700

    Agrego base para tests (sin implementar)

--- scala/src/test/scala/ProjectSpec.scala
+++ scala/src/test/scala/ProjectSpec.scala
@@ -9,6 +9,83 @@ class ProjectSpec extends FreeSpec with Matchers {
         Prueba.materia shouldBe "tadp"
       }
     }
+    "Pesca: Vikingo carga 50% de su peso + el doble de barbosidad. " - {
+      "Incrementa 5% su hambre" in {
+
+      }
+    }
+    "Combate: vikingo barbosidad + daño item." - {
+      "Incrementa 10% su hambre" in {
+
+      }
+    }
+    "Carrera vikingo mas veloz." - {
+      "Incrementa hambre 1% por kilometro" in {
+
+      }
+    }
+    "Patapez" - {
+      "No puede participar Posta si hambre > 50%" in {
+
+      }
+      "Doble de hambre luego de posta" in {
+
+      }
+    }
+    "Hipo" - {
+      "item sistema de vuelo" in {
+
+      }
+    }
+    "Astrid" - {
+      "item hacha aumenta 30 puntos daño" in {
+
+      }
+    }
+    "Patan" - {
+      "item maza aumenta 100 puntos daño" in {
+
+      }
+    }
+
+    "Dragones" - {
+      "Chimuelo: velocidad triple que dragon base" in {
+
+      }
+      "Nadder: produce 150 de daño" in {
+
+      }
+      "Gronckle: velocidad base a la mitad. Daño es 5 veces su peso" in {
+
+      }
+    }
+
+    "Jinetes" - {
+      //Jinete exitoso y fracasado (Sin causar efecto sobre draon o vikingo). Dragon puede cargar el 20% de su peso
+      //Chimuelo: vikingo tenga sistema vuelo
+      //Nadder: vikingo tengo daño menor
+      //Gronckle: vikingo no supere peso X
+      //
+      //Pesca jinete: Cargan tanto como carga posible dragon - peso vikingo
+      //Combate jinete: Suma daño dragon y daño vikingo
+      //Carrera jinete velocidad dragon - 1km/h por kilo vikingo
+      //Para estos tres anteriores hambre vikingo +5%
+      //(estos si tienen efecto)
+    }
+
+    //Pesca: Puede existir requerimiento de peso minimo a levantar para el participante
+    //Combate: Debe tener al menos X grado de barbaridad o arma equipada
+    //Carrera: Puede requerir uso de montura
+    //(No pueden participar vikingos que luego de este, lleguen al 100% de hambre). Descartar vikingos no cumplan pre-requisitos. Efecto sobre participantes. Resultado de la posta
+    //
+    //Ganador posta
+    //Varios participantes en posta
+    //
+    //mejor montura
+    //torneo varios
+    //reglas varias
   }
 
 }
+
+
