0a898efe669f40302e1968c1c3898562b829f756
enzo-mnc <enzo.mnc@gmail.com>
2020-07-02 21:37:16 -0300

    Agrego los tipos de item

--- scala/src/main/scala/items/Item.scala
+++ scala/src/main/scala/items/Item.scala
@@ -1,12 +1,27 @@
 package items
 
-trait Item{
+trait Item {
   val damage: Int = 0
   val energy: Double = 0
+  val itemType: ItemType
 }
 
-case class Weapon(override val damage: Int) extends Item
+case class Weapon(override val damage: Int) extends Item{
+  override val itemType: ItemType = WeaponType
+}
 
-case object FlySystem extends Item
+case object FlySystem extends Item {
+  override val itemType: ItemType = FlySystemType
+}
 
-case class Edible(override val energy: Double) extends Item
+case class Edible(override val energy: Double) extends Item {
+  override val itemType: ItemType = EdibleType
+}
+
+
+
+trait ItemType
+
+case object WeaponType extends ItemType
+case object EdibleType extends ItemType
+case object FlySystemType extends ItemType
